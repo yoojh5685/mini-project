1055eda63971c0ea968c1fe738f1e1f6030a5c52
yoojh5685 <yoojh5685@gmail.com>
2022-04-24 23:38:30 +0900

    Make&Load

--- manager.c
+++ manager.c
@@ -40,8 +40,10 @@ void saveData(Product p[], int count){
 
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
-	
+	for(int i=0; i<count; i++){
+		if(p[i].price ==-1) continue;
+	fprintf(fp,"%d %d %s\n", p[i].weight ,p[i].price, p[i].name);
+	}
 	fclose(fp);
 	printf("저장됨!\n");
 }
@@ -53,10 +55,20 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
+fp = fopen("product.txt", "rt");
+	if(fp==NULL){
+		printf("\n=>파일없음\n");
+		return 0;
+	}
+	else{
+		for(;count<100; count++){
+			fscanf(fp, "%d", &p[count].weight);	
+			if(feof(fp)) break;	
+			fscanf(fp, "%d", &p[count].price);
+			fscanf(fp, "%[^\n]s", p[count].name);
+		}
+	fclose(fp);
+	}
 	printf("=> 로딩 성공!\n");
 	return count;
 }
